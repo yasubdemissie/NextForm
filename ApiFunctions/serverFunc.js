7a3865984f22cce0b69b72ab6960e5b96367cb48
yasubdemissie <yasdam777@gmail.com>
2024-11-14 16:43:57 +0300

    data addition handled

--- ApiFunctions/serverFunc.js
+++ ApiFunctions/serverFunc.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 
 import { helperGetComment, helperGetPath } from "@/app/api/feedback/route";
 import { revalidatePath } from 'next/cache';
+import prisma from '@/_lib/_base';
 
 export async function submitAction(prevState, formData) {
   const email = formData.get("email");
@@ -20,6 +21,29 @@ export async function submitAction(prevState, formData) {
   data.push(comment);
 
   fs.writeFileSync(filepath, JSON.stringify(data));
+
+  revalidatePath('/');
+
+  return { message: "Feedback received", data: comment };
+}
+
+export async function formAction(prevState, formData) {
+
+  const title = formData.get("title");
+  const content = formData.get("content");
+
+  const comment = {
+    content,
+    title,
+  };
+  console.log(comment);
+
+  const data = await prisma.post.create({
+    data: comment,
+  });
+
+  console.log(data);
+
 
   revalidatePath('/');
 
--- Components/Form2.jsx
+++ Components/Form2.jsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { submitAction } from "@/ApiFunctions/serverFunc";
+import { submitAction, formAction as submitter } from "@/ApiFunctions/serverFunc";
 import { useActionState } from "react";
 
 function FormPage() {
-  const [state, formAction] = useActionState(submitAction, {});
+  const [state, formAction, isPending] = useActionState(submitter, {});
 
   return (
     <div className="flex flex-col row-span-11 justify-center items-center bg-gray-100 p-0">
@@ -12,33 +12,36 @@ function FormPage() {
         <form action={formAction}>
           <div className="mb-4">
             <label
-              htmlFor="email"
+              htmlFor="title"
               className="block text-sm font-medium text-gray-700"
             >
-              Email:
+              Title:
             </label>
             <input
-              type="email"
-              id="email"
-              name="email"
+              type="text"
+              id="title"
+              name="title"
+              placeholder="Title"
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
           </div>
           <div className="mb-4">
             <label
-              htmlFor="feedback"
+              htmlFor="content"
               className="block text-sm font-medium text-gray-700"
             >
-              Feedback:
+              Content:
             </label>
             <textarea
-              id="feedback"
-              name="feedback"
+              id="content"
+              name="content"
+              placeholder="Write your post here"
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             ></textarea>
           </div>
           <button
             type="submit"
+            disabled={isPending}
             className="w-full px-4 py-2 bg-indigo-600 text-white font-medium rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             Submit
